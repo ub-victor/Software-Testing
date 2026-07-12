cb3302e59ddec3f372adad7c79adc0650f59bf50
ub-victor <victoireushindi371@gmail.com>
2026-07-12 23:38:28 +0200

    Intinial Commit

--- studentregistration/src/main/java/auca/student/App.java
+++ studentregistration/src/main/java/auca/student/App.java
@@ -91,7 +91,7 @@ public class App
      */
     public int[] getEvenNumbers(int[] numbers) {
         if (numbers == null || numbers.length == 0) {
-            return null;
+            return new int[0];
         }
 
         int count = 0;
